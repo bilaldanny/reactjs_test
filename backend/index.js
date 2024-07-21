9875adcfb6cf67cde6291758a60ce452a2f11a43
muhammadbilal123 <bilaldanny@gmail.com>
2024-07-21 17:38:21 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,24 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+app.get('/get_data', async (req, res) => {
+    try {
+        const value = req.body.value;
+        
+        // Find data in MongoDB
+        const data = await RiskScore.findOne({ riskScore: value }).select('-_id nigerianStocks foreignStocks techStocks emergingStocks nigerianBonds foreignBonds commodities realEstate tBills alternative');
+    
+        if (data) {
+            res.json(data);
+        } else {
+            res.status(404).json({ message: 'Data not found' });
+        }
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+})
+
 app.post('/get_data', async (req, res) => {
     try {
         const value = req.body.value;
